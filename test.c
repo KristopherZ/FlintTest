5edb7119c7e88c3746856c842ba6b92c37554bae
kris <zhaohaonan1228@gmail.com>
2025-06-26 09:35:28 +0800

    computing partial sum of -L'/L

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <flint/arb.h>
+#include <flint/dirichlet.h>
 
 
 long vonMangolt1(long x) {
@@ -21,43 +22,71 @@ long vonMangolt1(long x) {
 
 int main() {
 
-    arb_t sigma;
-    arb_init(sigma);
-    arb_set_d(sigma, 1.1);
+    dirichlet_group_t G;
 
-    arb_t sum;
-    arb_init(sum);
+    ulong q = 4;
 
-    long len = 10000000;
+    long len = 100000;
 
     long prec = 100;
 
-    for (long i = 1; i <= len; i++) {
+    dirichlet_group_init(G, q);
 
-        long n = vonMangolt1(i);
-        if (n != 1) {
-            arb_t num;
-            arb_init(num);
-            arb_log_ui(num, n,  prec);
+    dirichlet_char_t chi;
+    dirichlet_char_init(chi, G);
+    dirichlet_char_next_primitive(chi, G);
+    do {
 
-            arb_t den;
-            arb_init(den);
+        if (dirichlet_char_is_real(G, chi)) {
 
-            arb_t b;
-            arb_init(b);
-            arb_set_si(b, i);
+            arb_t sigma;
+            arb_init(sigma);
+            arb_set_d(sigma, 1.1);
 
-            arb_pow(den, b, sigma, prec);
+            arb_t sum;
+            arb_init(sum);
 
-            arb_t term;
-            arb_init(term);
-            arb_div(term, num, den, prec);
+            for (long i = 1; i <= len; i++) {
 
-            arb_add(sum, sum, term, prec);
+                long val = dirichlet_chi(G, chi, i);
+
+                if (val != - 1) {
+                    long n = vonMangolt1(i);
+                    if (n != 1) {
+                        arb_t num;
+                        arb_init(num);
+                        arb_log_ui(num, n,  prec);
+
+                        arb_t den;
+                        arb_init(den);
+
+                        arb_t b;
+                        arb_init(b);
+                        arb_set_si(b, i);
+
+                        arb_pow(den, b, sigma, prec);
+
+                        arb_t term;
+                        arb_init(term);
+                        arb_div(term, num, den, prec);
+
+                        if (val == 0) {
+                            arb_add(sum, sum, term, prec);
+                        }else {
+                            arb_neg(term, term);
+                            arb_add(sum, sum, term, prec);
+                        }
+
+
+                    }
+                }
+
+            }
+            arb_print(sum);
         }
 
-    }
-    arb_print(sum);
+    } while (dirichlet_char_next_primitive(chi, G) >= 0);
+
     return 0;
 }
 
